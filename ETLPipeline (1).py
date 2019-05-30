b08cd0439fb17c21f019966b2a7bcce2cb3c7a9d
mudigosa <shanmukhamudigonda@gmail.com>
2019-05-30 16:37:20 -0400

    Update ETLPipeline (1).py

--- ETLPipeline (1).py
+++ ETLPipeline (1).py
@@ -1,120 +1,87 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import sys\n",
-    "import pandas as pd\n",
-    "from sqlalchemy import create_engine\n",
-    "\n",
-    "def load_data(messages_filepath, categories_filepath):\n",
-    "    '''\n",
-    "    input:\n",
-    "        messages_filepath: The path of messages dataset.\n",
-    "        categories_filepath: The path of categories dataset.\n",
-    "    output:\n",
-    "        df: The merged dataset\n",
-    "    '''\n",
-    "    disastermessages = pd.read_csv('disaster_messages.csv')\n",
-    "    disastermessages.head()\n",
-    "    # load categories dataset\n",
-    "    disastercategories = pd.read_csv('disaster_categories.csv')\n",
-    "    disastercategories.head()\n",
-    "    df = pd.merge(disastermessages, disastercategories, left_on='id', right_on='id', how='outer')\n",
-    "    return df\n",
-    "\n",
-    "def clean_data(df):\n",
-    "    '''\n",
-    "    input:\n",
-    "        df: The merged dataset in previous step.\n",
-    "    output:\n",
-    "        df: Dataset after cleaning.\n",
-    "    '''\n",
-    "    disastercategories = df.disastercategories.str.split(';', expand = True)\n",
-    "    # select the first row of the categories dataframe\n",
-    "    row = disastercategories.iloc[0,:]\n",
-    "\n",
-    "    # use this row to extract a list of new column names for categories.\n",
-    "    # one way is to apply a lambda function that takes everything \n",
-    "    # up to the second to last character of each string with slicing\n",
-    "    disastercategory_colnames = row.apply(lambda x:x[:-2])\n",
-    "    print(disastercategory_colnames)\n",
-    "    disastercategories.columns = category_colnames\n",
-    "    for column in disastercategories:\n",
-    "    # set each value to be the last character of the string\n",
-    "        disastercategories[column] = disastercategories[column].str[-1]\n",
-    "    \n",
-    "    # convert column from string to numeric\n",
-    "    disastercategories[column] = disastercategories[column].astype(np.int)\n",
-    "    disastercategories.head()\n",
-    "    df.drop('categories', axis = 1, inplace = True)\n",
-    "    df = pd.concat([df, categories], axis = 1)\n",
-    "    # drop the original categories column from `df`\n",
-    "    df = df.drop('categories',axis=1)\n",
-    "\n",
-    "    df.head()\n",
-    "    # check number of duplicates\n",
-    "    print('Number of duplicated rows: {} out of {} samples'.format(df.duplicated().sum(),df.shape[0]))\n",
-    "    df.drop_duplicates(subset = 'id', inplace = True)\n",
-    "    return df\n",
-    "\n",
-    "def save_data(df, database_filepath):\n",
-    "    engine = create_engine('sqlite:///DisasterResponse.db')\n",
-    "    df.to_sql('df', engine, index=False)\n",
-    "\n",
-    "def main():\n",
-    "    if len(sys.argv) == 4:\n",
-    "\n",
-    "        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]\n",
-    "\n",
-    "        print('Loading data...\\n    MESSAGES: {}\\n    CATEGORIES: {}'\n",
-    "              .format(messages_filepath, categories_filepath))\n",
-    "        df = load_data(messages_filepath, categories_filepath)\n",
-    "\n",
-    "        print('Cleaning data...')\n",
-    "        df = clean_data(df)\n",
-    "\n",
-    "        print('Saving data...\\n    DATABASE: {}'.format(database_filepath))\n",
-    "        save_data(df)\n",
-    "\n",
-    "        print('Cleaned data saved to database!')\n",
-    "\n",
-    "    else:\n",
-    "        print('Please provide the filepaths of the messages and categories '\\\n",
-    "              'datasets as the first and second argument respectively, as '\\\n",
-    "              'well as the filepath of the database to save the cleaned data '\\\n",
-    "              'to as the third argument. \\n\\nExample: python process_data.py '\\\n",
-    "              'disaster_messages.csv disaster_categories.csv '\\\n",
-    "              'DisasterResponse.db')\n",
-    "\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "main()"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    input:
+        messages_filepath: The path of messages dataset.
+        categories_filepath: The path of categories dataset.
+    output:
+        df: The merged dataset
+    '''
+    disastermessages = pd.read_csv('disaster_messages.csv')
+    disastermessages.head()
+    # load categories dataset
+    disastercategories = pd.read_csv('disaster_categories.csv')
+    disastercategories.head()
+    df = pd.merge(disastermessages, disastercategories, left_on='id', right_on='id', how='outer')
+    return df
+
+def clean_data(df):
+    '''
+    input:
+        df: The merged dataset in previous step.
+    output:
+        df: Dataset after cleaning.
+    '''
+    disastercategories = df.disastercategories.str.split(';', expand = True)
+    # select the first row of the categories dataframe
+    row = disastercategories.iloc[0,:]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    disastercategory_colnames = row.apply(lambda x:x[:-2])
+    print(disastercategory_colnames)
+    disastercategories.columns = category_colnames
+    for column in disastercategories:
+    # set each value to be the last character of the string
+        disastercategories[column] = disastercategories[column].str[-1]
+    
+    # convert column from string to numeric
+    disastercategories[column] = disastercategories[column].astype(np.int)
+    disastercategories.head()
+    df.drop('categories', axis = 1, inplace = True)
+    df = pd.concat([df, categories], axis = 1)
+    # drop the original categories column from `df`
+    df = df.drop('categories',axis=1)
+
+    df.head()
+    # check number of duplicates
+    print('Number of duplicated rows: {} out of {} samples'.format(df.duplicated().sum(),df.shape[0]))
+    df.drop_duplicates(subset = 'id', inplace = True)
+    return df
+
+def save_data(df, database_filepath):
+    engine = create_engine('sqlite:///DisasterResponse.db')
+    df.to_sql('df', engine, index=False)
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+main()
